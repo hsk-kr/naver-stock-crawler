5a97d4faa78c48e32bacf84b60b4c8f8abf235d9
hsk-kr <hsk.coder@gmail.com>
2020-05-17 05:58:28 +0000

    Implement crawler API to execute the crawler

--- api_server/api/urls.py
+++ api_server/api/urls.py
@@ -5,6 +5,7 @@ from .views import (
     StockViewSet,
     CrawlerViewSet,
     LogViewSet,
+    CrawlerExecutionView
 )
 
 api_router = routers.SimpleRouter()
@@ -12,4 +13,4 @@ api_router.register(r"stock", StockViewSet)
 api_router.register(r"crawler", CrawlerViewSet)
 api_router.register(r"log", LogViewSet)
 
-urlpatterns = [path("", include(api_router.urls))]
+urlpatterns = [path("", include(api_router.urls)), path("crawler_execution", CrawlerExecutionView.as_view())]
--- api_server/api/views.py
+++ api_server/api/views.py
@@ -1,4 +1,9 @@
-import rest_framework
+import subprocess
+from django import forms
+from django.views import View
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
 
 from .models import Stock, Crawler, Log
@@ -23,3 +28,37 @@ class CrawlerViewSet(viewsets.ModelViewSet):
 class LogViewSet(viewsets.ModelViewSet):
     serializer_class = LogSerializer
     queryset = Log.objects.all()
+
+class CrawlerExecutionValidationForm(forms.Form):
+    start_date = forms.CharField()
+    end_date = forms.CharField()
+
+p_crawler = None  # Global variable used in CrawlerView to execute crawler only one
+@method_decorator(csrf_exempt, name="dispatch")
+class CrawlerExecutionView(View):
+    def get(self, request):
+        global p_crawler
+        if p_crawler != None and p_crawler.poll() == None:
+            return JsonResponse({"msg": "already executed wait till ending of the process"}, status=226)
+        else:
+            return JsonResponse({"msg": "ready to execute"}, status=200)
+
+    def post(self, request):
+        form = CrawlerExecutionValidationForm(request.POST)
+
+        if not form.is_valid():
+            return JsonResponse({"msg": "Bad request"}, status=400)
+
+        global p_crawler
+
+        if p_crawler != None:
+            if p_crawler.poll() == None:
+                return JsonResponse({"msg": "already executed wait till ending of the process"}, status=226)
+            else:
+                p_crawler = None
+        proc_params = ["python", "../crawler/crawler.py"]
+        proc_params.extend([request.POST["start_date"], request.POST["end_date"]])
+        p_crawler = subprocess.Popen(
+            proc_params, stdout=subprocess.PIPE)
+
+        return JsonResponse({"msg": "success to execute"}, status=202)
