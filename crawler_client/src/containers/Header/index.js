347def029b2fbae646e18d866898c1d245eb4f63
hsk-kr <hsk.coder@gmail.com>
2020-05-20 07:08:51 +0000

    Apply nav activate

--- crawler_client/src/containers/Header/index.js
+++ crawler_client/src/containers/Header/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import classNames from "classnames/bind";
 import { Navbar, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import styles from "./styles.scss";
+
+const cx = classNames.bind(styles);
 
 const Header = () => {
   return (
@@ -8,8 +13,16 @@ const Header = () => {
       <Navbar.Toggle aria-controls="header" />
       <Navbar.Collapse id="header">
         <Nav className="mr-auto">
-          <Nav.Link href="/crawler">Crawler</Nav.Link>
-          <Nav.Link href="#b">History</Nav.Link>
+          <Nav.Link>
+            <NavLink to="/crawler" activeClassName="selected">
+              Cralwer
+            </NavLink>
+          </Nav.Link>
+          <Nav.Link>
+            <NavLink to="/b" activeClassName="selected">
+              Stocks
+            </NavLink>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
